61301a59e0477c51940062e840c225c1469e0c39
OmerYilmazlar <yilmazlarfarukomer@gmail.com>
2026-04-04 22:01:09 +0100

    feat: replace carousel london set with london_view images

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 import { BrowserRouter, Link, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import cpcHands from './assets/images/home/cpc-hands.jpg'
 import harleyStreet from './assets/images/home/harley-street.jpg'
-import london1 from './assets/images/home/london1.jpeg'
-import london2 from './assets/images/home/london2.jpeg'
-import londonBest from './assets/images/home/london-best.jpeg'
+import londonView1 from './assets/images/home/london-view1.jpg'
+import londonView2 from './assets/images/home/london-view2.jpg'
+import londonView3 from './assets/images/home/london-view3.jpg'
 import sevenLionYard from './assets/images/home/seven-lion-yard-2.webp'
 import tpdLogo from './assets/images/affiliations/turkish-psychological-association.png'
 import bpsLogo from './assets/images/affiliations/british-psychological-society.png'
@@ -81,7 +81,7 @@ const stockImages = {
   therapyShowcase: sectionPsychologySessionMart,
 }
 
-const homeCarouselImages = [londonBest, london1, london2, cpcHands, harleyStreet, sevenLionYard]
+const homeCarouselImages = [londonView1, londonView2, londonView3, cpcHands, harleyStreet, sevenLionYard]
 
 const seoByPath = {
   '/': {
